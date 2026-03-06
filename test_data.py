c472cc4ca07b603dc3a2ed95a24e488b761be737
Jeshz-Dev <jezreeleguillermo@gmail.com>
2026-03-06 16:33:53 +0800

    minor tweaksters :>

--- test_data.py
+++ test_data.py
@@ -8,5 +8,26 @@ def test_json(filepath):
             print(f"File name: {file.name} is valid")
             return True
     except Exception as e:
-        print(f"Error: {e}")
+        print(f"Error: {file.name} {e}")
         return False
+    except FileNotFoundError as e:
+        print(f"File {file.name} was not found.")
+        return False
+    except json.JSONDecodeError as e:
+        print(f"{file.name} has error: {e}")
+        return False;
+
+data_dir = Path("data")
+files = ["resources.json", "professionals.json", "hotlines.json"]
+
+print("Testing if this file works:>")
+
+all_valid = True
+for filename in files:
+    if not test_json(data_dir/filename):
+        all_valid = False
+
+if all_valid:
+    print("The shit you created worked! :'>")
+else:
+    print("You gotta work harder man:<")
