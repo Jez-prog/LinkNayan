5b78c1a31db46bd286e6fffe3170700b8a922c09
Jeshz-Dev <jezreeleguillermo@gmail.com>
2026-03-18 23:45:37 +0800

    initial ai.py

--- src/linknayan/ai.py
+++ src/linknayan/ai.py
@@ -9,25 +9,32 @@ client = OpenAI(
     base_url= OPENROUTER_BASE_URL,
     api_key = OPENROUTER_API_KEY,
 )
-crisi_keywords = {
+
+
+
+def detect_crisis(text):
+    text_lower = text.lower()
+    
+    crisis_keywords = {
         "english": [
-            "suicide", "kill myself","end my life", "don't want to live",
-        "harm myself", "self harm", "cutting", "overdose", "jump",
-        "want to die", "going to die", "dead", "hopeless forever"
+            "suicide", "kill myself", "end my life", "don't want to live",
+            "harm myself", "self harm", "cutting", "overdose", "jump",
+            "want to die", "going to die", "dead", "hopeless forever"
         ],
-        "tagalog":[
+        "tagalog": [
             "gusto kong mamatay", "papatayin ko sarili ko", "suicide",
-        "ayoko nang mabuhay", "walang pag-asa", "sasaktan ko sarili ko",
-        "mag-overdose", "magsusuffer lang", "mamatay na lang"
+            "ayoko nang mabuhay", "walang pag-asa", "sasaktan ko sarili ko",
+            "mag-overdose", "magsusuffer lang", "mamatay na lang"
         ]
     }
 
+    for lang in crisis_keywords:
+        for keyword in crisis_keywords[lang]:
+            if keyword in text_lower:
+                return True
 
-def detect_crisis():
-    text_lower = text.lower()
+    return False
 
 
-    
-if __name__ == "__name__":
-    print(detect_crisis("I want to kill myslef."))
-    
+if __name__ == "__main__":
+    print(detect_crisis("I want to kill myself."))
